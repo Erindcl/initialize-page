c1f8ba29d332169a25f9840a695f0cad99df8822
Erindcl <cailian_erin@163.com>
2019-07-08 22:33:53 +0800

    添加：文件与路径输入提示并获取输入值

--- bin/init.js
+++ bin/init.js
@@ -1,38 +1,43 @@
 #! /usr/bin/env node
 
 const program = require('commander');
-const colors = require('colors');
-const inquirer = require('inquirer');
-const { resolveApp } = require('../config/defaultPaths');
+const colors = require('colors');  // 提示有颜色
+const inquirer = require('inquirer');  // 终端文字提示 并获取交互信息
+
+const { realpathSync } = require('fs');  // node fs模块 提供文件相关操作API
+const appDirectory = realpathSync(process.cwd()); // 获取项目的绝对路径
+function resolveApp(relativePath) {  // 根据相对路径获取绝对路径
+    return resolve(appDirectory, relativePath);
+}
+
 // const init = require('../script/init.js');
 const logs = console.log;
 program
     .option('-p, --path', '自定义生成目录')
-    .option('-t, --ts', '支持typescript')
     .parse(process.argv);
 
 try {
     let question = [{
         type: 'Input',
-        name: 'swagger',
-        message: '请输入swagger地址(如:http://xxxxx/swagger-ui.html#/)',
+        name: 'fileName',
+        message: '请输入文件名',
     }] 
     program.path && question.push({
         type: 'Input',
         name: 'path',
-        message: '请输入生成目录(绝对路径)',
+        message: '请输入生成目录(当前项目为根目录,默认为src/pages/)',
         default: ''
     })
     inquirer
         .prompt(question)
         .then((answers) => {
             if (answers.swagger == '') {
-                logs(colors.red('请输入swagger地址'));
+                logs(colors.red('请输入文件名'));
             } else {
                 if(!answers.path || answers.path == ''){
-                    answers.path = resolveApp('src/api/');
+                    answers.path = resolveApp('src/pages/');
                 }
-                // init(answers.swagger, answers.path, program.ts)
+                // init(answers.swagger, answers.path)
             }
         })
 } catch (err) {
